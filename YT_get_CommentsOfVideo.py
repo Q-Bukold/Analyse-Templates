1f6efe112d1a4f5c41bcf46d3890523b4b753268
Q-Bukold <83951996+Q-Bukold@users.noreply.github.com>
2023-01-28 20:51:09 +0100

    created refined version
    1. Number of Comments can be set
    2. Output DF is prettier

--- YT_get_CommentsOfVideo.py
+++ YT_get_CommentsOfVideo.py
@@ -3,56 +3,77 @@ import googleapiclient
 from googleapiclient.discovery import build
 import pandas as pd
 
-
-def load_comments(match):
-    for item in match["items"]:
-        comment = item["snippet"]["topLevelComment"]
-        author = comment["snippet"]["authorDisplayName"]
-        text = comment["snippet"]["textOriginal"]
-        list_all_comments.append(text)
-        authors.append(author)
-
-        print("Comment by {}: {}".format(author, text))
-        if 'replies' in item.keys():
-            for reply in item['replies']['comments']:
-                rauthor = reply['snippet']['authorDisplayName']
-                rtext = reply["snippet"]["textOriginal"]
-            print("\n\tReply by {}: {}".format(rauthor, rtext), "\n")
-
-
-def get_comment_threads(youtube, video_id, nextPageToken):
-    results = youtube.commentThreads().list(
-        part="snippet",
-        maxResults=100, #CHANGE
-        videoId=video_id,
-        textFormat="plainText",
-        pageToken=nextPageToken
-    ).execute()
-    return results
-
-
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 api_service_name = "youtube"
 api_version = "v3"
 DEVELOPER_KEY = "AIzaSyAYvRpVKJUS5MUnw6NVcIQB484ao6CdutE"
-video_id = "hEMm7gxBYSc"
-youtube = googleapiclient.discovery.build(
+video_id = "XFkzRNyygfk" #creep
+youtube_api = googleapiclient.discovery.build(
     api_service_name, api_version, developerKey=DEVELOPER_KEY)
-list_all_comments = []
-authors = []
 
-match = get_comment_threads(youtube, video_id, '')
-next_page_token = match["nextPageToken"]
-load_comments(match)
+def get_comment(api, video, page_token):
+    #create list of lists
+    page_list = []
 
-try:
-    while next_page_token:
-        match = get_comment_threads(youtube, video_id, next_page_token)
-        next_page_token = match["nextPageToken"]
-        load_comments(match)
-except:
-    data = pd.DataFrame(list_all_comments, index=authors, columns=["Comments"])
-    print(data)
+    #get page
+    page = api.commentThreads().list(
+        part="snippet",
+        maxResults=5, #CHANGE
+        videoId=video_id,
+        textFormat="plainText",
+        #is there a netxt page? If yes, get token:
+        pageToken=page_token,
+        order="time" # "time" or "relevance" (not the same as TOP-Comments on YT, but similar)
+    ).execute()
+
+    for item in page["items"]:
+        item_list = []
+        comment = item["snippet"]["topLevelComment"]
+        author = comment["snippet"]["authorDisplayName"]
+        text = comment["snippet"]["textOriginal"]
+        #print("Comment by {}: {}".format(author, text))
+        item_list.append(author)
+        item_list.append(text)
+        page_list.append(item_list)
+
+        '''
+        if 'replies' in item.keys():
+            for reply in item['replies']['comments']:
+                rauthor = reply['snippet']['authorDisplayName']
+                rtext = reply["snippet"]["textOriginal"]
+            #print("\n\tReply by {}: {}".format(rauthor, rtext), "\n")
+        '''
+
+    next_page_token = page["nextPageToken"]
+    print(page_list)
+
+    return page_list, next_page_token
 
 
-data.to_csv('data.csv')
+
+data = []
+next_page_token = ''
+is_first_page = True
+i = 1
+i_max = 5 # N of comments = i_max * maxResults
+
+while is_first_page or next_page_token and i <= i_max:
+    i += 1
+    is_first_page = False
+    page_data, next_page_token = get_comment(youtube_api, video_id, next_page_token)
+    data += page_data
+
+
+
+data_df = pd.DataFrame(data, columns=['author', 'comment'])
+
+#post processing
+data_df['comment'] = data_df['comment'].replace(r'\s+|\\n', ' ', regex=True) 
+data_df['comment'] = data_df['comment'].str.strip()
+
+
+
+print(data_df)
+print(data_df.shape)
+
+data_df.to_csv('/Users/qbukold/Desktop/Schnulzen/gonewiththewind_comments2.tsv', sep="\t")
